db9c24a85fc0582edb9a23344e82496b86926332
Thomas Cavalli <thomaspcavalli@gmail.com>
2024-06-17 15:28:57 -0400

    Added insert a word into the letter grid.

--- app/src/main/java/com/thomascavalli/word_search_puzzle_creator/MainActivity.kt
+++ app/src/main/java/com/thomascavalli/word_search_puzzle_creator/MainActivity.kt
@@ -11,9 +11,11 @@
 //  1) Created by Thomas Cavalli on 06/01/2024.
 //  2) Added Scroll view to the main activity by Thomas Cavalli on 06/06/2024.
 //  3) Created first diagonal grid by Thomas Cavalli on 06/09/2024.
-//  4) Started coding growable grid by Thomas Cavalli on 06/09/2024.
-//  5)
-//  6)
+//  4) Created coding growable grid by Thomas Cavalli on 06/10/2024.
+//  5) Created coding to insert a word into the grid by Thomas Cavalli on 06/17/2024.
+//  6) Started coding to create the Word Search Puzzle by Thomas Cavalli on 06/18/2024.
+//  7)
+//  8)
 
 package com.thomascavalli.word_search_puzzle_creator
 
@@ -43,6 +45,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -68,12 +71,38 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 
 object Constants {
-    const val creator = "CC_BY_4.0, Created by Thomas Cavalli on 06/01/2024."
+    const val CREATOR = " Word Search Puzzle Creator "
+    const val CREATOR1 = " CC_BY_4.0, Created by Thomas Cavalli on 06/01/2024. "
+    const val CREATOR2 = " What Changed by who and when.  Thank you. "
+}
+object Word {
+    var word: String = ""
+    var letters: List<Char> = listOf()
+    var selected: Boolean = false
+    var selectRow: List<Int> = listOf()  // 0..<rowMaxSize
+    var selectCol: List<Int> = listOf()  // 0..<columnMaxSize
+    var priority: Int = 0
+    var growTop: Int = 0
+    var growBottom: Int = 0
+    var growLeft: Int = 0
+    var growRight: Int = 0
+    var startRow: Int = 0
+    var startCol: Int = 0
+    var index: Int = 0
+    var direction: Int = 0
+    var selectedItems: List<Int> = listOf()
 }
 object Global {
     var letters = mutableListOf<Char>()
     var rowMaxSize: Int = 25
     var columnMaxSize: Int = 15
+    var aWord: Word = Word
+    var bWord: Word = Word
+    val dRow = intArrayOf (-1,-1, 0, 1, 1, 1, 0,-1)
+    val dCol = intArrayOf ( 0, 1, 1, 1, 0,-1,-1,-1)
+    var bGrid = mutableListOf<Char>()
+    var aGrid = mutableListOf<Char>()
+    var Words = mutableListOf<Word>()
 }
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -133,18 +162,23 @@ fun DrawGrid(words: MutableList<String>, newGrid: Boolean = false) {
                 val luckyWord = biggestWords.random()
                 Global.rowMaxSize = luckyWord.length
                 Global.columnMaxSize = luckyWord.length
-                Global.letters.removeAll { true }
+                Global.letters.clear()   //removeAll { true }
                 val count = Global.rowMaxSize * Global.columnMaxSize
                 repeat(count) {
                     Global.letters.add('*')
                 }
-                var rowCounter = Global.rowMaxSize - 1
-                var columnCounter = Global.columnMaxSize - 1
-                for (letter in luckyWord) {
-                    Global.letters[rowCounter * Global.columnMaxSize + columnCounter] = letter
-                    rowCounter--
-                    columnCounter--
-                }
+//                var rowCounter = Global.rowMaxSize - 1
+//                var columnCounter = Global.columnMaxSize - 1
+//                for (letter in luckyWord) {
+//                    Global.letters[rowCounter * Global.columnMaxSize + columnCounter] = letter
+//                    rowCounter--
+//                    columnCounter--
+//                }
+                val theWord = luckyWord.toList()
+                Global.bWord = Word
+                makeWord(Global.rowMaxSize - 1, Global.columnMaxSize - 1, theWord, 7)
+                Global.Words.clear()
+                Global.Words.add(Global.bWord)
             }
 
             val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
@@ -170,7 +204,7 @@ fun DrawGrid(words: MutableList<String>, newGrid: Boolean = false) {
 }
 @Composable
 fun RandomLetterGrid(rows: Int = 5, columns: Int = 5) {
-    val letters = generateRandomLetters(rows * columns)
+    Global.letters = generateRandomLetters(rows * columns)
     val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     val darkTheme = isSystemInDarkTheme()
     val colorScheme = when {
@@ -179,20 +213,20 @@ fun RandomLetterGrid(rows: Int = 5, columns: Int = 5) {
         darkTheme -> MaterialTheme.colorScheme
         else -> MaterialTheme.colorScheme
     }
-
+    Text("Random Grid")
     LazyVerticalGrid(
         columns = GridCells.Fixed(columns),
         verticalArrangement = Arrangement.spacedBy(2.dp),
         horizontalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        items(letters) { letter ->
+        items(Global.letters) { letter ->
             LetterCard(letter = letter, colorScheme = colorScheme)
         }
     }
 }
 
 @Composable
-fun LetterCard(letter: Char, colorScheme: androidx.compose.material3.ColorScheme) {
+fun LetterCard(letter: Char, colorScheme: ColorScheme) {
     Text(
         text = letter.toString(),
         fontSize = 20.sp,
@@ -207,123 +241,153 @@ fun EditableWordList(searchWordSaver: SearchWordSaver, words: MutableList<String
     var addOrConfirm: String by remember { mutableStateOf("Add") }
     var clearOrCancel: String by remember { mutableStateOf("Clear") }
     var newGrid: Boolean by remember { mutableStateOf(true) }
+    var showView: String by remember { mutableStateOf("Words") }
     Column(
         modifier = Modifier
             .padding(2.dp)
             .fillMaxWidth()
-            .fillMaxHeight(fraction = 0.5f),
+            .fillMaxHeight(fraction = 1f),
     ) {
+        if (showView == "Words") {
+            newGrid = true
+            newGrid = false
+            newGrid = true
+            Button(
+                modifier = Modifier.padding(2.dp).align(Alignment.CenterHorizontally),
+                onClick = {
+                    showView = "Grid"
+                }) {
+                Text("Create Word Search")
+            }
+            // Input field for adding new words
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    value = newWord,
+                    onValueChange = { newWord = it },
+                    label = { Text("Add Words here.") },
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
 
-        Row(modifier = Modifier
-            .padding(2.dp)
-            .fillMaxWidth()
-            .fillMaxHeight(fraction = 0.1f),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(onClick = {
-                growTop()
-                newWord = "A"
-                newWord = ""
-                newGrid = false
-            }) {
-                Text("Grow Top")
-            }
-            Button(onClick = {
-                growBottom()
-                newWord = "A"
-                newWord = ""
-                newGrid = false
-            }) {
-                Text("Bottom")
-            }
-            Button(onClick = {
-                growLeft()
-                newWord = "A"
-                newWord = ""
-                newGrid = false
-            }) {
-                Text("Left")
-            }
-            Button(onClick = {
-                growRight()
-                newWord = "A"
-                newWord = ""
-                newGrid = false
-            }) {
-                Text("Right")
-            }
-        }
-        // Input field for adding new words
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            OutlinedTextField(
-                value = newWord,
-                onValueChange = { newWord = it },
-                label = { Text("Add Words here.") },
-                modifier = Modifier.weight(1f)
-            )
-            Spacer(modifier = Modifier.padding(8.dp))
-
-            Button(onClick = {
-                if (addOrConfirm == "Add") {
-                    if (newWord.isNotBlank()) {
-                        words.add(newWord)
-                        words.sort()
+                Button(onClick = {
+                    if (addOrConfirm == "Add") {
+                        if (newWord.isNotBlank()) {
+                            words.add(newWord)
+                            words.sort()
+                            newWord = ""
+                            searchWordSaver.saveSearchWords(words)
+                            newGrid = true
+                        }
+                    } else {
+                        newWord = "A"
                         newWord = ""
+                        words.removeAll { true }
                         searchWordSaver.saveSearchWords(words)
+                        addOrConfirm = "Add"
+                        clearOrCancel = "Clear"
                         newGrid = true
                     }
-                }else{
-                    newWord = "A"
-                    newWord = ""
-                    words.removeAll { true }
-                    searchWordSaver.saveSearchWords(words)
-                    addOrConfirm = "Add"
-                    clearOrCancel = "Clear"
-                    newGrid = true
-                }
-            }) {
+                }) {
                     Text(addOrConfirm)
-            }
-            Spacer(modifier = Modifier.padding(8.dp))
-
-            Button(onClick = {
-                if (clearOrCancel == "Clear") {
-                    addOrConfirm = "Confirm"
-                    clearOrCancel = "Cancel"
-                }else{
-                    clearOrCancel = "Clear"
-                    addOrConfirm = "Add"
                 }
-            }) {
-                Text(clearOrCancel)
+                Spacer(modifier = Modifier.padding(8.dp))
+
+                Button(onClick = {
+                    if (clearOrCancel == "Clear") {
+                        addOrConfirm = "Confirm"
+                        clearOrCancel = "Cancel"
+                    } else {
+                        clearOrCancel = "Clear"
+                        addOrConfirm = "Add"
+                    }
+                }) {
+                    Text(clearOrCancel)
+                }
+            }
+            // Display the list of words
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(words) { word ->
+                    WordItem(word = word, onDelete = {
+                        words.remove(word)
+                        searchWordSaver.saveSearchWords(words)
+                        newWord = "A"
+                        newWord = ""
+                        addOrConfirm = "Add"
+                        clearOrCancel = "Clear"
+                    })
+                }
             }
         }
 
-        // Display the list of words
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            items(words) { word ->
-                WordItem(word = word, onDelete = {
-                    words.remove(word)
-                    searchWordSaver.saveSearchWords(words)
-                    newWord = "A"
-                    newWord = ""
-                    addOrConfirm = "Add"
-                    clearOrCancel = "Clear"
-                })
+        if (showView == "Grid") {
+            Column(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight(fraction = 1f),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Button(onClick = {
+                    showView = "Words"
+                }) {
+                    Text("< Back to Word List")
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxWidth()
+                        .fillMaxHeight(fraction = 0.1f),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Button(onClick = {
+                        growTop()
+                        newWord = "A"
+                        newWord = ""
+                        newGrid = false
+                        newGrid = true
+                        newGrid = false
+                    }) {
+                        Text("Grow Top")
+                    }
+                    Button(onClick = {
+                        growBottom()
+                        newWord = "A"
+                        newWord = ""
+                        newGrid = false
+                        newGrid = true
+                        newGrid = false
+                    }) {
+                        Text("Bottom")
+                    }
+                    Button(onClick = {
+                        growLeft()
+                        newWord = "A"
+                        newWord = ""
+                        newGrid = false
+                        newGrid = true
+                        newGrid = false
+                    }) {
+                        Text("Left")
+                    }
+                    Button(onClick = {
+                        growRight()
+                        newWord = "A"
+                        newWord = ""
+                        newGrid = false
+                        newGrid = true
+                        newGrid = false
+                    }) {
+                        Text("Right")
+                    }
+                }
+                DrawGrid(words = words, newGrid = newGrid)
             }
         }
-    }
-    Column(modifier = Modifier
-        .padding(2.dp)
-        .fillMaxWidth()
-        .fillMaxHeight(fraction = 1f),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        DrawGrid(words = words, newGrid = newGrid)
     }
 }
 
@@ -356,7 +420,7 @@ fun isSystemADarkTheme(): Color {
         Color.DarkGray
 }
 
-private fun generateRandomLetters(count: Int): List<Char> {
+private fun generateRandomLetters(count: Int): MutableList<Char> {
     val chars = mutableListOf<Char>()
     repeat(count) {
         chars.add(('A'..'Z').random())
@@ -388,6 +452,26 @@ private fun growRight() {
     }
     Global.letters.add('*')
     Global.columnMaxSize++
+}
+private fun makeWord(bRow: Int, bColumn: Int, theWord: List<Char>, bDirection: Int) {
+    var aRow = bRow
+    var aColumn = bColumn
+    Global.bWord.startRow = aRow
+    Global.bWord.startCol = aColumn
+    Global.bWord.direction = bDirection
+    Global.bWord.selectRow = listOf()
+    Global.bWord.selectCol = listOf()
+    Global.bWord.selectedItems = listOf()
+    for (letter in theWord) {
+        Global.bWord.word += letter.toString()
+        Global.bWord.letters += letter
+        Global.bWord.selectedItems += aRow * Global.columnMaxSize + aColumn
+        Global.letters[aRow * Global.columnMaxSize + aColumn] = letter
+        Global.bWord.selectRow += aRow
+        Global.bWord.selectCol += aColumn
+        aRow += Global.dRow[bDirection]
+        aColumn += Global.dCol[bDirection]
+    }
 }
 class SearchWordSaver(private val context: Context) {
     private val fileName = "search_words.txt"
